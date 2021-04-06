097050a95346551f34f5e8193337aa0b7a65ae43
zhangwanping <zhangwanping@tpson.cn>
2021-04-06 20:04:57 +0800

    三级联动

---
+++ app/src/main/java/com/example/nativeplacechoose/GetJsonDataUtil.java
@@ -0,0 +1,31 @@
+package com.example.nativeplacechoose;
+
+import android.content.Context;
+import android.content.res.AssetManager;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * @author zwp
+ * @description: 读取本地Json的工具类GetJsonDataUtil
+ * @date: 2021/4/6 15:02
+ */
+public class GetJsonDataUtil {
+    public String getJson(Context context, String fileName) {
+        StringBuilder stringBuilder = new StringBuilder();
+        try {
+            AssetManager assetManager = context.getAssets();
+            BufferedReader bf = new BufferedReader(new InputStreamReader(
+                    assetManager.open(fileName)));
+            String line;
+            while ((line = bf.readLine()) != null) {
+                stringBuilder.append(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return stringBuilder.toString();
+    }
+}
